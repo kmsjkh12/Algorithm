0ec9ffa784624d0b5b3a935681a98fa543f6c2fe
kmsjkh12 <121033327+kmsjkh12@users.noreply.github.com>
2023-09-19 17:23:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.38 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,50 @@
 import java.util.*;
+
+
 class Solution {
-    int dx [] = {-1, 0, 1, 0};
-    int dy [] = {0, 1, 0 ,-1};
+    static boolean visited[][];
+    static int count = 0 ;
+    static int dx[] = {-1,0,1,0};
+    static int dy[] = {0,-1,0,1};
     
-    boolean visited [][];
-    public int solution(int [][] maps) {
+    
+    public int solution(int[][] maps) {
         int answer = 0;
+    
+        visited= new boolean[maps.length][maps[0].length];
         
-        visited = new boolean[maps.length][maps[0].length];
-        return bfs(0,0,maps);
+        
+        bfs(0,0,maps);
+        
+        answer = maps[maps.length-1][maps[0].length-1];
+        if(answer==1){
+            answer=-1;
+        }
+        return answer;
     }
     
-    
-    private int bfs(int x, int y, int [][] maps){
-        Queue<int []> queue= new LinkedList<>();
-
-        queue.offer(new int[] {x,y});
-        while(!queue.isEmpty()){
-
-            int [] temp = queue.remove();
-            int temp_x = temp[0];
-            int temp_y = temp[1];
+    static void bfs(int x,int y, int [][] m){
+        
+        Queue<int [] > q = new LinkedList<>();
+        q.add(new int [] {x,y});
+        
+        while(!q.isEmpty()){
+            int now[] = q.poll();
             
-        for(int i = 0;i <4; i++){
-            int nx = temp_x+ dx[i];    
-            int ny = temp_y+ dy[i];   
-            if(nx<0 || nx > maps.length -1|| ny<0 || ny > maps[0].length-1)
-                continue;
-            
-            if(!visited[nx][ny] && maps[nx][ny]!=0){
-                maps[nx][ny]=maps[temp_x][temp_y]+1;
-                queue.offer(new int [] {nx,ny});
-                visited[nx][ny]= true;
-
+            for(int i = 0 ; i < 4; i++){
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                
+                if(nx >=0 && nx < m.length && ny >= 0 && ny< m[0].length
+                  &&!visited[nx][ny] && m[nx][ny] == 1 
+                  ){
+                    visited[nx][ny]=true;
+                    
+                    m[nx][ny] = m[now[0]][now[1]] + 1;
+                    q.add(new int []{nx,ny});
+                }
             }
-            
         }
-        }
-        
-        
-      int result =  maps[maps.length-1][maps[0].length-1];
-      if(result ==1){
-          return -1;
-          
-      }
-        else{
-return result;}
         
     }
 }
