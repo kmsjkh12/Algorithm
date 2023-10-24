3e8d6222d95258b67e811536493c9e59fd7ad348
kmsjkh12 <121033327+kmsjkh12@users.noreply.github.com>
2023-10-24 12:09:11 +0900

    [Silver III] Title: 바이러스, Time: 136 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,49 +1,56 @@
-import java.io.*;
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static boolean visited [];
-	static int arr [][];
-	static int count;
-	static int N,M,V;
 	
-	public static void main(String[] args) throws IOException {
-
-		Scanner sc = new Scanner(System.in);
+	public static void main (String[]args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
 		
-		N= sc.nextInt();
-		M= sc.nextInt();
-		arr =  new int[N+1][N+1];
-		
-		for(int i = 0 ; i <M; i++) {
-			int a =sc.nextInt();
-			int b= sc.nextInt();
-			arr[a][b]=1;
-			arr[b][a]=1;
+		st = new StringTokenizer(br.readLine());
+		int count = 0;
+		int M = Integer.parseInt(st.nextToken());
+		Queue<Integer> q = new LinkedList<>();
+		int arr [][] =new int [N+1][N+1];
+		boolean visited [] =new boolean [N+1];
+		for(int i= 0 ; i < M ;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a][b] = 1;
+			arr[b][a] = 1;
 		}
 		
-		visited = new boolean[N+1];
-		dfs(1);
-		System.out.print(count);
+		q.add(1);
+		visited[1]=true;
 		
-	}
-
-	private static void dfs(int t) {
-		// TODO Auto-generated method stub
-		visited[t]= true;
-		
-		for(int i = 1; i <N+1; i++) {
-			if(!visited[i] && arr[t][i]==1) {
-				dfs(i);
-				count++;
-
+		while(!q.isEmpty()) {
+			int a = q.poll();
+			for(int i = 2 ; i <=N; i++) {
+				if(!visited[i] && arr[a][i] == 1) {
+					visited[i]=true;
+					q.add(i);
+					count++;
+				}
 			}
+			
 		}
-	}
-
+		
+		System.out.println(count);
+		
 	
-	
+		
+		
+		
+		
+		
+	}	
 
 	
 }
