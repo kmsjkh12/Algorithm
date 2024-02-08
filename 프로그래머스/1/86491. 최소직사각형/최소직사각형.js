39e3034a99d2adf35645affeb4bc3c68fd5502f5
kmsjkh12 <121033327+kmsjkh12@users.noreply.github.com>
2024-02-08 15:38:14 +0900

    [level 1] Title: 최소직사각형, Time: 5.53 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,20 @@
+function solution(sizes) {
+  let longer = [];
+    let shorter = [];
+
+    for(let i = 0; i < sizes.length; i++) {
+        if(sizes[i][0] > sizes[i][1]) {
+            longer.push(sizes[i][0]);
+            shorter.push(sizes[i][1]);
+        } else {
+            longer.push(sizes[i][1]);
+            shorter.push(sizes[i][0]);
+        }
+    }
+
+    let biggerSideMax = Math.max(...longer);
+    let smallerSideMax = Math.max(...shorter);
+    
+    return biggerSideMax * smallerSideMax;
+}
+
