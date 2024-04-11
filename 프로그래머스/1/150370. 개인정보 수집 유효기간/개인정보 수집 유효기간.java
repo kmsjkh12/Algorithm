9aa4ffd5f82d264e13cca9edb49d690b93b662f1
kmsjkh12 <121033327+kmsjkh12@users.noreply.github.com>
2024-04-11 12:13:50 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.88 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,32 +1,54 @@
-import java.util.*;
+import java.time.LocalDate;
+import java.util.HashMap;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
-        int date = getDate(today);
+        int[] answer = {};
+        int[] result=new int[privacies.length];
+        int count=0;
+        HashMap<String,Integer> term=new HashMap<>();
+        for(int i=0;i<terms.length;i++){
+            String[] temp=terms[i].split(" ");//A, 6으로 분할
+            term.put(temp[0],Integer.valueOf(temp[1]));
+        }
+        //A:6, B:12, C:3
 
-        for(String a: terms){
-            String [] term = a.split(" ");
-            map.put(term[0], Integer.parseInt(term[1]));
+        String[] t=today.split("\\.");
+        int[] nows=new int[3];
+        for(int i=0;i<3;i++){
+            nows[i]=Integer.valueOf(t[i]);
         }
-        
-        for(int i = 0 ; i < privacies.length;i++){
-            String privace[] = privacies[i].split(" ");
-        
-            if (getDate(privace[0]) + (map.get(privace[1]) * 28) <= date) {
-                answer.add(i + 1);
+        LocalDate now=LocalDate.of(nows[0],nows[1],nows[2]);
+
+        for(int i=0;i<privacies.length;i++){
+            String[] temp=privacies[i].split(" ");
+            String day=temp[0];
+            String name=temp[1];
+
+            int during=term.get(name);
+
+            String[] s=day.split("\\.");
+            int[] signs=new int[3];
+            for(int j=0;j<3;j++){
+                signs[j]=Integer.valueOf(s[j]);
             }
+            LocalDate signday=LocalDate.of(signs[0],signs[1],signs[2]);
+            //만기일은 사인한 날 하루전에 만기
+            LocalDate expire=signday.plusMonths(during).minusDays(1);
+
+            //오늘 날짜가 만기일보다 크면, 삭제해야함
+            if(now.isAfter(expire)){
+                result[count]=i+1;
+                count++;
+            }
+
         }
-        return answer.stream().mapToInt(integer -> integer).toArray();
+
+        answer=new int[count];
+        for(int i=0;i<count;i++){
+            answer[i]=result[i];
+        }
         
-    }
-    
-     private int getDate(String today) {
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-        return (year * 12 * 28) + (month * 28) + day;
+        return answer;
     }
 }
