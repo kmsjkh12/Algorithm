bceb1a6c3df951f5e31793aa3128434e48169a81
kmsjkh12 <121033327+kmsjkh12@users.noreply.github.com>
2024-06-20 16:58:47 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.67 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 79.6 MB, 시간: 1.57 ms
+메모리: 79.9 MB, 시간: 1.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 15:43:51
+2024년 06월 20일 16:58:43
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,56 +1,56 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<Integer>[] arr;
+    static ArrayList<Integer>[] graph;
     static int min;
     public int solution(int n, int[][] wires) {
         int answer = -1;
         
-        arr = new ArrayList[ n + 1 ];
+        graph= new ArrayList[ n + 1 ];
         min = Integer.MAX_VALUE;
         
-        for( int i = 1; i <= n; i++ ){
-            arr[i] =new ArrayList<>();
+        for(int i = 1 ; i<= n ; i++){
+            graph[i]= new ArrayList<>();
         }
         
-        for(int i = 0; i< wires.length; i++){
+        for(int i = 0 ; i < wires.length ;i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
-            
-            arr[v1].add(v2);
-            arr[v2].add(v1);
+            graph[v1].add(v2);
+            graph[v2].add(v1);
         }
-        
-        for(int i = 0; i< wires.length; i++){
+         for(int i = 0 ; i < wires.length ;i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
+          
+            boolean visited [] = new boolean[ n + 1]; 
             
-            boolean[] visited = new boolean[n+1];
-            
-            arr[v1].remove(Integer.valueOf(v2));
-            arr[v2].remove(Integer.valueOf(v1));
-            
-            int cnt = dfs(1, visited);
-            
-            int diff = Math.abs(cnt - ( n - cnt ));
-            min =Math.min(diff,min);
-            arr[v1].add(v2);
-            arr[v2].add(v1);
+            graph[v1].remove(Integer.valueOf(v2));
+            graph[v2].remove(Integer.valueOf(v1));
+             
+             
+            int cnt = dfs( 1, visited);
+             
+             
+            int diff = Math.abs(cnt - (n - cnt));
+             
+              min = Math.min(min, diff);
+             graph[v1].add(v2);
+             graph[v2].add(v1);
         }
-        
-        
-        
         return min;
     }
-    public static int dfs(int v , boolean [] visited ){
-        visited[v] =true;
-        int cnt = 1;
+    
+    public int dfs( int c , boolean[] visited){
+        visited[c] = true;
+        int count = 1; 
         
-        for( int i : arr[v]){
-            if(!visited[i]){
-                cnt +=dfs(i, visited);
+        for(int next : graph[c]){
+            if(!visited[next]){
+                count += dfs(next, visited);
+                
             }
         }
-        return cnt;
+        return count;
     }
 }
